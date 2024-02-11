72c7a7de6df0f8bc09041c39c3d93d32c607de5c
Hayden <hwalker@windevsystems.us>
2024-02-11 05:17:12 +0000

    coverage bullshit

--- test/controllers/ip_ban_controller_test.rb
+++ test/controllers/ip_ban_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class IpbanControllerTest < ActionDispatch::IntegrationTest
+class IpBanControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,21 @@
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
+require "simplecov"
+SimpleCov.start
+
+require 'simplecov-cobertura'
+SimpleCov.formatter = SimpleCov::Formatter::CoberturaFormatter
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+end
+require 'rake/testtask'
+Rake::TestTask.new(:test) do |test|
+  test.libs << 'lib' << 'test'
+  test.pattern = 'test/**/*_test.rb'
+  test.verbose = true
 end
